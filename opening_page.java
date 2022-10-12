cb3391feddcd78bf4f4a07dd82c4916c5f1148d3
Roee Belkin <roeebelkin@Roees-MacBook-Pro.local>
2022-10-12 15:25:55 -0500

    adjusted panel size

--- opening_page.java
+++ opening_page.java
@@ -1,11 +1,11 @@
 import java.awt.*;  
 import javax.swing.*;  
-public class PanelExample {  
-     PanelExample()  
+public class opening_page {  
+     opening_page()  
         {  
         JFrame f= new JFrame("Panel Example");    
         JPanel panel=new JPanel();  
-        panel.setBounds(40,80,200,200);    
+        panel.setBounds(10,10,1000,600);    
         panel.setBackground(Color.gray);  
         JButton b1=new JButton("Button 1");     
         b1.setBounds(50,100,80,30);    
@@ -15,12 +15,12 @@ public class PanelExample {
         b2.setBackground(Color.green);   
         panel.add(b1); panel.add(b2);  
         f.add(panel);  
-                f.setSize(400,400);    
+                f.setSize(1010,610);    
                 f.setLayout(null);    
                 f.setVisible(true);    
         }  
         public static void main(String args[])  
         {  
-        new PanelExample();  
+        new opening_page();  
         }  
     }  

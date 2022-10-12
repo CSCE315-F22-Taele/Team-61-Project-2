85edb45e6ee729b5eba1a9bbd740814f5e3a7f6e
Roee Belkin <roeebelkin@Roees-MacBook-Pro.local>
2022-10-12 16:26:42 -0500

    Added manager page which allows you to switch between pages

---
+++ Manager_Home_Page.java
@@ -0,0 +1,34 @@
+import java.awt.*;  
+import javax.swing.*;  
+import java.awt.event.*; 
+
+public class Manager_Home_Page {
+    JFrame f = new JFrame("Home Page");
+    Manager_Home_Page(){  
+       //JFrame f= new JFrame("Panel Example");    
+       JPanel panel=new JPanel();  
+       panel.setBounds(10,10,1000,600);    
+       JButton b1=new JButton("Back To Home Page");     
+       b1.setBounds(50,100,80,30);    
+        b1.addActionListener(new ActionListener(){  
+                public void actionPerformed(ActionEvent e){  
+                    b1.setBackground(Color.green);  
+                        new Manager_Page(); 
+                        f.dispose(); 
+                    }  
+            }); 
+       panel.add(b1);  
+       f.add(panel);  
+               f.setSize(1010,610);    
+               f.setLayout(null);    
+               f.setVisible(true);    
+       }  
+       
+       public void windowClosing (WindowEvent e) {    
+           f.dispose();    
+       }    
+       public static void main(String args[])  
+       {  
+       new Manager_Home_Page();  
+       }  
+}

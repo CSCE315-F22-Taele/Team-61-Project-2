2fa27dc040ca8fad07ef7a3de3a5f9de5ae171f9
Roee Belkin <roeebelkin@Roees-MacBook-Pro.local>
2022-10-12 17:44:29 -0500

    Orders made connects to database to get proper sale id

--- Clerk_Home_Page.java
+++ Clerk_Home_Page.java
@@ -1,14 +1,26 @@
 import java.awt.*;  
 import javax.swing.*;  
 import java.awt.event.*; 
+import java.util.*; 
 
 public class Clerk_Home_Page {
+    Vector<Order> orders = new Vector<Order>(); 
     JFrame f = new JFrame("Home Page");
+    boolean additional_entree = false; 
     Clerk_Home_Page(){  
-       //JFrame f= new JFrame("Panel Example");    
+       //JFrame f= new JFrame("Panel Example");
+       GridLayout test_layout = new GridLayout(4, 4); 
        JPanel panel=new JPanel();  
+       panel.setLayout(test_layout); 
        panel.setBounds(10,10,1000,600);    
-       JButton b1=new JButton("Back To Home Page");     
+       JButton b1=new JButton("Back To Home Page");
+       //b1.setBackground(Color.red);     
+       //b1.setOpaque(true);
+       //b1.setBorderPainted(false);
+       JButton b2= new JButton("Bowl"); 
+       JButton b3= new JButton("Burrito"); 
+       JButton b4= new JButton("Taco"); 
+       JButton b5= new JButton("Salad"); 
        b1.setBounds(50,100,80,30);    
         b1.addActionListener(new ActionListener(){  
                 public void actionPerformed(ActionEvent e){  
@@ -17,7 +29,24 @@ public class Clerk_Home_Page {
                         f.dispose(); 
                     }  
             }); 
+        b2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                Order current_order; 
+                if (additional_entree){
+                    current_order = new Order(orders.lastElement().Sale_Id); 
+                }
+                else{
+                    current_order = new Order(); 
+                }
+                current_order.Entree = "Bowl"; 
+                orders.add(current_order); 
+            }
+        });
        panel.add(b1);  
+       panel.add(b2);
+       panel.add(b3);
+       panel.add(b4); 
+       panel.add(b5); 
        f.add(panel);  
                f.setSize(1010,610);    
                f.setLayout(null);    
---
+++ Order.java
@@ -0,0 +1,81 @@
+import java.sql.*; 
+import java.io.*;
+import java.util.*;
+import java.time.*;    
+import java.text.*; 
+import java.util.Date;
+
+import javax.security.sasl.Sasl;
+
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+
+public class Order {
+    int Sale_Id; 
+    String date; 
+    String Entree; 
+    String Protein;
+    int salsa;
+    int guac; 
+    int queso;
+    int drink;
+    float cost; 
+
+    Order(int saleid){
+        Date temp_date = new Date(); 
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd");
+        date = formatter.format(temp_date);
+        Entree = ""; 
+        Protein = "";
+        salsa = 0;
+        guac = 0;
+        queso = 0;
+        drink = 0; 
+        cost = 0; 
+        Sale_Id = saleid; 
+    }
+
+    Order(){
+        Date temp_date = new Date(); 
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd");
+
+        // connecting to our data base to get sale id start number
+        Connection conn = null;
+        String teamNumber = "61"; // Your team number
+        String sectionNumber = "905"; // Your section number
+        String dbName = "csce331_" + sectionNumber + "_" + teamNumber;
+        String dbConnectionString = "jdbc:postgresql://csce-315-db.engr.tamu.edu/" + dbName;
+        dbSetup myCredentials = new dbSetup(); 
+
+        try {
+            conn = DriverManager.getConnection(dbConnectionString, dbSetup.user, dbSetup.pswd);
+          } 
+        catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+        try{
+            Statement stmt = conn.createStatement(); 
+            String sqlQuery = "SELECT MAX(sale_id) from cabo_grill_sales;";
+            ResultSet result = stmt.executeQuery(sqlQuery); 
+            result.next(); 
+            Sale_Id = result.getInt("max") + 1; 
+            System.out.println(Sale_Id);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            //System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+        date = formatter.format(temp_date);
+        Entree = ""; 
+        Protein = "";
+        salsa = 0;
+        guac = 0;
+        queso = 0;
+        drink = 0; 
+        cost = 0; 
+    }
+}

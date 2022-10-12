bb2a21c0e80c0a2afb966762d07df748b564b8a8
justinsingletary <justinsingletary@tamu.edu>
2022-10-12 17:26:25 -0500

    Added JList for Manager Page

--- Manager_Home_Page.java
+++ Manager_Home_Page.java
@@ -3,32 +3,41 @@ import javax.swing.*;
 import java.awt.event.*; 
 
 public class Manager_Home_Page {
+
     JFrame f = new JFrame("Home Page");
+    DefaultListModel<String> l1 = new DefaultListModel<>(); 
+
     Manager_Home_Page(){  
-       //JFrame f= new JFrame("Panel Example");    
-       JPanel panel=new JPanel();  
-       panel.setBounds(10,10,1000,600);    
-       JButton b1=new JButton("Back To Home Page");     
-       b1.setBounds(50,100,80,30);    
+        //JFrame f= new JFrame("Panel Example");    
+        JPanel panel=new JPanel();  
+        panel.setBounds(10,10,1000,600);    
+        JButton b1=new JButton("Back To Home Page");     
+        b1.setBounds(50,100,80,30);    
         b1.addActionListener(new ActionListener(){  
-                public void actionPerformed(ActionEvent e){  
-                    b1.setBackground(Color.green);  
-                        new Home_Page(); 
-                        f.dispose(); 
-                    }  
-            }); 
-       panel.add(b1);  
-       f.add(panel);  
-               f.setSize(1010,610);    
-               f.setLayout(null);    
-               f.setVisible(true);    
-       }  
-       
-       public void windowClosing (WindowEvent e) {    
-           f.dispose();    
-       }    
-       public static void main(String args[])  
-       {  
-       new Manager_Home_Page();  
-       }  
+            public void actionPerformed(ActionEvent e){  
+                b1.setBackground(Color.green);  
+                    new Home_Page(); 
+                    f.dispose(); 
+                }  
+        }); 
+        panel.add(b1);  
+
+        DefaultListModel<String> l1 = new DefaultListModel<>();  
+        l1.addElement("Item1");  
+        l1.addElement("Item2");  
+        l1.addElement("Item3");  
+        l1.addElement("Item4");  
+        JList<String> list = new JList<>(l1);  
+        list.setBounds(100,100, 75,75);  
+        f.add(list); 
+        
+        f.add(panel);  
+        f.setSize(1010,610);    
+        f.setLayout(null);    
+        f.setVisible(true);   
+    }  
+    public static void main(String args[])  
+    {  
+        new Manager_Home_Page();  
+    }  
 }

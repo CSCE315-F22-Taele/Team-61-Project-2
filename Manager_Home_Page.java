0fb3a7766f7903e48f62eca7b930834534006811
Brandon Moon <brandonmoon@client-10-229-69-254.tamulink.tamu.edu>
2022-10-12 17:26:07 -0500

    attempted to create a drop down

--- Manager_Home_Page.java
+++ Manager_Home_Page.java
@@ -4,12 +4,14 @@ import java.awt.event.*;
 
 public class Manager_Home_Page {
     JFrame f = new JFrame("Home Page");
-    Manager_Home_Page(){  
-       //JFrame f= new JFrame("Panel Example");    
-       JPanel panel=new JPanel();  
-       panel.setBounds(10,10,1000,600);    
-       JButton b1=new JButton("Back To Home Page");     
-       b1.setBounds(50,100,80,30);    
+    Manager_Home_Page(){     
+       JPanel panel = new JPanel();  
+       panel.setBounds(10,10,1000,600);   
+
+       JButton b1 = new JButton("Back To Home Page");     
+       b1.setBounds(50,100,80,30);
+
+        // On click, goes back to home page and turns green    
         b1.addActionListener(new ActionListener(){  
                 public void actionPerformed(ActionEvent e){  
                     b1.setBackground(Color.green);  
@@ -17,6 +19,7 @@ public class Manager_Home_Page {
                         f.dispose(); 
                     }  
             }); 
+
        panel.add(b1);  
        f.add(panel);  
                f.setSize(1010,610);    
@@ -24,11 +27,23 @@ public class Manager_Home_Page {
                f.setVisible(true);    
        }  
        
-       public void windowClosing (WindowEvent e) {    
-           f.dispose();    
-       }    
+
+       
+       String[] entreeOptions = {"All", "Burrito", "Bowl", "Taco", "None"};
+       JComboBox<String> cb = new JComboBox<String>(entreeOptions);
+       
+    //    String getEntreeType = (String) JOptionPane.showInputDialog(
+    //             null,
+    //             "Choose an entree type",
+    //             "Choose Entree Type",
+    //             JOptionPane.QUESTION_MESSAGE,
+    //             null,
+    //             optionsToChoose,
+    //             optionsToChoose[4]);
+
+
        public static void main(String args[])  
        {  
-       new Manager_Home_Page();  
+            new Manager_Home_Page();  
        }  
 }

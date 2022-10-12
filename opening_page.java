dd441dccadb16130c022e6f8d6823e3cc48d02a4
Brandon Moon <brandonmoon@client-10-229-69-254.tamulink.tamu.edu>
2022-10-12 16:51:35 -0500

    updated my branch with main branch

--- opening_page.java
+++ opening_page.java
@@ -4,29 +4,23 @@ import java.awt.event.*;
 public class opening_page {  
      opening_page()  
         {  
-        JFrame f= new JFrame("Panel Example");    
-        JPanel panel=new JPanel();  
+        JFrame f = new JFrame("Panel Example");    
+        JPanel panel = new JPanel();  
+
         panel.setBounds(10,10,1000,600);    
-        panel.setBackground(Color.gray);  
-        JButton b1=new JButton("Button 1");    
+        panel.setBackground(Color.gray);
+
+        JButton b1 = new JButton("Button 1");     
         b1.setBounds(50,100,80,30);    
-        b1.setBackground(Color.RED);
-        b1.setOpaque(true);
-        b1.setBorderPainted(false);
+        b1.setBackground(Color.red);
         b1.addActionListener(new ActionListener(){  
                 public void actionPerformed(ActionEvent e){  
-<<<<<<< HEAD
-                            b1.setBackground(Color.GREEN);  
-                        }  
-                    }); 
-=======
                     b1.setBackground(Color.green);  
                     }  
             }); 
->>>>>>> eaeeb537bbf6c1166becb60bec9e9cc9e471e1a6
         JButton b2=new JButton("Button 2");   
         b2.setBounds(100,100,80,30);    
-        b2.setBackground(Color.RED);   
+        b2.setBackground(Color.red);   
         panel.add(b1); panel.add(b2);  
         f.add(panel);  
                 f.setSize(1010,610);    
@@ -35,6 +29,6 @@ public class opening_page {
         }  
         public static void main(String args[])  
         {  
-        new opening_page();
+        new opening_page();  
         }  
     }  

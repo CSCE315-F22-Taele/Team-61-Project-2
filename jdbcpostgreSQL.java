99f150195049f1f41ac250bf037bc4f77928ea61
justinsingletary <justinsingletary@tamu.edu>
2022-10-13 17:02:28 -0500

    cleaned up changes

--- jdbcpostgreSQL.java
+++ jdbcpostgreSQL.java
@@ -42,12 +42,8 @@ public class jdbcpostgreSQL {
 					this.statement = "SELECT * FROM cabo_grill_sales WHERE entree_type = '" + entree + "';";
 					this.totalAmount = "SELECT SUM(cost) FROM cabo_grill_sales WHERE entree_type = '" + entree + "';";
 				} else {
-<<<<<<< HEAD
 					this.statement = "SELECT * FROM cabo_grill_sales WHERE entree_type = '" + entree + "' AND date BETWEEN '" + start + "' AND '" + end + "';";
 					this.totalAmount = "SELECT SUM(cost) FROM cabo_grill_sales WHERE entree_type = '" + entree + "' AND date BETWEEN '" + start + "' AND '" + end + "';";
-=======
-					this.statement = "SELECT * FROM cabo_grill_sales WHERE type = 'entree' AND date BETWEEN '" + start + "' AND '" + end + "';";
->>>>>>> main
 				}
 			}
 			//Select all with Particular Entree and Protein
@@ -56,12 +52,8 @@ public class jdbcpostgreSQL {
 					this.statement = "SELECT * FROM cabo_grill_sales WHERE entree_type = '" + entree + "' AND protein = '" + protein + "';";
 					this.totalAmount = "SELECT SUM(cost) FROM cabo_grill_sales WHERE entree_type = '" + entree + "' AND protein = '" + protein + "';";
 				} else {
-<<<<<<< HEAD
 					this.statement = "SELECT * FROM cabo_grill_sales WHERE entree_type = '" + entree + "' AND date BETWEEN '" + start + "' AND '" + end + "';";
 					this.totalAmount = "SELECT SUM(cost) FROM cabo_grill_sales WHERE entree_type = '" + entree + "' AND date BETWEEN '" + start + "' AND '" + end + "';";
-=======
-					this.statement = "SELECT * FROM cabo_grill_sales WHERE type = 'protein' AND date BETWEEN '" + start + "' AND '" + end + "';";
->>>>>>> main
 				}
 			}
 			//Select all with Particular Entree and Side
@@ -70,7 +62,6 @@ public class jdbcpostgreSQL {
 					this.statement = "SELECT * FROM cabo_grill_sales WHERE entree_type = '" + entree + "' AND " + side + "= 1;";
 					this.totalAmount = "SELECT SUM(cost) FROM cabo_grill_sales WHERE entree_type = '" + entree + "' AND " + side + "= 1;";
 				} else {
-<<<<<<< HEAD
 					this.statement = "SELECT * FROM cabo_grill_sales WHERE entree_type = '" + entree + "' AND " + side + "= 1 AND date BETWEEN '" + start + "' AND '" + end + "';";
 					this.totalAmount = "SELECT SUM(cost) FROM cabo_grill_sales WHERE entree_type = '" + entree + "' AND " + side + "= 1 AND date BETWEEN '" + start + "' AND '" + end + "';";
 				}
@@ -83,9 +74,6 @@ public class jdbcpostgreSQL {
 				} else {
 					this.statement = "SELECT * FROM cabo_grill_sales WHERE entree_type = '" + entree + "' AND protein = '" + protein + "'" + " AND " + side + "= 1 AND date BETWEEN '" + start + "' AND '" + end + "';";
 					this.totalAmount = "SELECT SUM(cost) FROM cabo_grill_sales WHERE entree_type = '" + entree + "' AND protein = '" + protein + "'" + " AND " + side + "= 1 AND date BETWEEN '" + start + "' AND '" + end + "';";
-=======
-					this.statement = "SELECT * FROM cabo_grill_sales WHERE entree_type = '" + entree + "' AND date BETWEEN '" + start + "' AND '" + end + "';";
->>>>>>> main
 				}
 			}
 
@@ -105,12 +93,8 @@ public class jdbcpostgreSQL {
 					this.statement = "SELECT * FROM cabo_grill_sales WHERE protein = '" + protein + "'" + " AND " + side + "= 1;";
 					this.totalAmount = "SELECT SUM(cost) FROM cabo_grill_sales WHERE protein = '" + protein + "'" + " AND " + side + "= 1;";
 				} else {
-<<<<<<< HEAD
 					this.statement = "SELECT * FROM cabo_grill_sales WHERE protein = '" + protein + "'" + " AND " + side + "= 1 AND date BETWEEN '" + start + "' AND '" + end + "';";
 					this.totalAmount = "SELECT SUM(cost) FROM cabo_grill_sales WHERE protein = '" + protein + "'" + " AND " + side + "= 1 AND date BETWEEN '" + start + "' AND '" + end + "';";
-=======
-					this.statement = "SELECT * FROM cabo_grill_sales WHERE protein = '" + protein + "' AND date BETWEEN '" + start + "' AND '" + end + "';";
->>>>>>> main
 				}
 			}
 
@@ -121,10 +105,7 @@ public class jdbcpostgreSQL {
 					this.totalAmount = "SELECT SUM(cost) FROM cabo_grill_sales WHERE " + side + " = 1;";
 				} else {
 					this.statement = "SELECT * FROM cabo_grill_sales WHERE " + side  + " = 1 AND date BETWEEN '" + start + "' AND '" + end + "'";
-<<<<<<< HEAD
 					this.totalAmount = "SELECT SUM(cost) FROM cabo_grill_sales WHERE " + side  + " = 1 AND date BETWEEN '" + start + "' AND '" + end + "'";
-=======
->>>>>>> main
 				}
 			}
 		}
@@ -155,7 +136,7 @@ public class jdbcpostgreSQL {
       		System.err.println(e.getClass().getName()+": "+e.getMessage());
       		System.exit(0);
     	}
-    	System.out.println("Opened database successfully");
+    	//System.out.println("Opened database successfully");
 
 
     	try {
@@ -202,71 +183,9 @@ public class jdbcpostgreSQL {
     	//closing the connection
     	try {
       		conn.close();
-      		System.out.println("Connection Closed.");
+      		//System.out.println("Connection Closed.");
     	} catch(Exception e) {
       		System.out.println("Connection NOT Closed.");
     	}
   	}
-
-
-	/*
-    String[] entree = new String[]{"burrito", "bowl", "tacos"};
-    String[] protein = new String[]{"chicken", "steak", "beef", "vegetable medley"};
-    Double[] protein_prices = new Double[]{8.50, 8.89, 8.79, 7.89};
-
-    Random random_num = new Random();
-    int num_entries = 20;
-    int count = 0;
-    int month = 1;
-    int day = 1;
-    int year = 2022;
-
-    for (int i = 0; i < num_entries; i++) {
-      if (count == 25) {
-        day += 1;
-        count = 0;
-      }
-      int sale_id = 1000 + i;
-      String date =  year + "-" + month + "-" + day;
-      int entree_index = random_num.nextInt(entree.length); //[0, entree.length)
-      int protein_index = random_num.nextInt(protein.length);
-      int chips_and_queso = random_num.nextInt(2);
-      int chips_and_salsa = random_num.nextInt(2);
-      int chips_and_guac = random_num.nextInt(2);
-      int num_drinks = random_num.nextInt(2);
-
-      double total_price = protein_prices[protein_index] + chips_and_queso*3.49 + chips_and_salsa*2.19 + chips_and_guac*3.69 + num_drinks*2.45;
-      double rounded_total = Math.round(total_price * 100.0) / 100.0;
-
-      String sql_entry = "INSERT INTO cabo_grill_sales (sale_id, date, entree_type, protein, chips_and_salsa, chips_and_queso, chips_and_guac, drink, cost) VALUES('"+
-      sale_id + "', '" + date + "', '" + entree[entree_index] + "', '" + protein[protein_index] + "', " + 
-      chips_and_salsa + ", " + chips_and_queso + ", " + chips_and_guac  + ", " + num_drinks + ", " + rounded_total +")\n";
-
-      //Print each entry
-      System.out.print(sql_entry);
-
-      try {
-        Statement stmt = conn.createStatement();
-        stmt.executeUpdate(sql_entry);
-      } 
-      catch (Exception e){
-        e.printStackTrace();
-        System.err.println(e.getClass().getName()+": "+e.getMessage());
-        System.exit(0);
-      }
-      count += 1;
-    }
-  
-    //closing the connection
-    try {
-      conn.close();
-      System.out.println("Connection Closed.");
-    } 
-    catch(Exception e) {
-      System.out.println("Connection NOT Closed.");
-    }//end try catch
-  //end main
-  }
-  */
 }
-//end Class

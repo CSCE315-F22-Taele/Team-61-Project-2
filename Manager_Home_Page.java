7228826f2d258be6200e1a5d5141a444d5dca91c
justinsingletary <justinsingletary@tamu.edu>
2022-10-13 10:45:38 -0500

    Changes

--- Manager_Home_Page.java
+++ Manager_Home_Page.java
@@ -7,20 +7,6 @@ public class Manager_Home_Page {
 
     JFrame f = new JFrame("Home Page");
 
-    /*
-    Manager_Home_Page(ArrayList<String> entrees, ArrayList<String> protein, ArrayList<String> sides) {   
-
-        JPanel panel = new JPanel();  
-        panel.setBounds(10, 10, 1000, 600); 
-
-        JButton b1 = new JButton("Back To Home Page");     
-        b1.setBounds(50,100,80,30);    
-        b1.addActionListener(new ActionListener() {  
-
-        DefaultListModel<String> l1 = new DefaultListModel<>(); 
-    */
-
-
     Manager_Home_Page(ArrayList<String> entrees, ArrayList<String> protein, ArrayList<String> sides) {   
 
         JPanel panel = new JPanel();  
@@ -31,9 +17,9 @@ public class Manager_Home_Page {
         b1.addActionListener(new ActionListener() {  
             public void actionPerformed(ActionEvent e){  
                 b1.setBackground(Color.green);  
-                    new Home_Page(); 
-                    f.dispose(); 
-                }  
+                new Home_Page(); 
+                f.dispose(); 
+            }  
         }); 
         panel.add(b1);  
 

6fe1debec07f371ee085253736e2312f8da40c24
Brandon Moon <brandonmoon@connect-172-31-52-96.vpn.tamu.edu>
2022-10-12 20:06:38 -0500

    Updated code to current main branch

--- Manager_Home_Page.java
+++ Manager_Home_Page.java
@@ -6,13 +6,15 @@ public class Manager_Home_Page {
 
     JFrame f = new JFrame("Home Page");
 
-    Manager_Home_Page(){     
-        JPanel panel=new JPanel();  
-        panel.setBounds(10,10,1000,600);    
+    Manager_Home_Page(String[] entrees, String[] protein, String[] sides) {   
+          
+        JPanel panel = new JPanel();  
+        panel.setBounds(10, 10, 1000, 600); 
 
-        JButton b1=new JButton("Back To Home Page");     
-        b1.setBounds(50,100,80,30);
-        // Redirects to home page when button is clicked    
+        JButton b1 = new JButton("Back To Home Page");     
+        b1.setBounds(50,100,80,30); 
+        
+        // On click - Goes back to home page
         b1.addActionListener(new ActionListener(){  
             public void actionPerformed(ActionEvent e){  
                 b1.setBackground(Color.green);  
@@ -22,41 +24,47 @@ public class Manager_Home_Page {
         }); 
         panel.add(b1);  
 
+
+        //Start Date TextBox
+        JTextArea startDate = new JTextArea();  
+        startDate.setBounds(180,100, 100,20);  
+        f.add(startDate);
+
+        //End Date TextBox
+        JTextArea endDate = new JTextArea();  
+        endDate.setBounds(300,100, 100,20);  
+        f.add(endDate);
+
+
+        //Lists
         DefaultListModel<String> l1 = new DefaultListModel<>();  
-        l1.addElement("All");  
-        l1.addElement("Burrito");  
-        l1.addElement("Bowl");  
-        l1.addElement("None");  
-        JList<String> list = new JList<>(l1);  
-        list.setBounds(100,100, 75,75);  
-        final JLabel label = new JLabel();  // label outputs what options are selected
-        label.setSize(500,100);
-        f.add(list); 
+        for (String e : entrees) {
+            l1.addElement(e);
+        }
+        JList<String> list1 = new JList<>(l1);  
+        list1.setBounds(420, 100, 100, 110);  
+        f.add(list1); 
 
-        
-        JButton b2 = new JButton("Submit");  
-        b2.setBounds(200,150,80,30);
+        DefaultListModel<String> l2 = new DefaultListModel<>();  
+        for (String p : protein) {
+            l2.addElement(p);
+        }
+        JList<String> list2 = new JList<>(l2);  
+        list2.setBounds(540, 100, 100, 110);  
+        f.add(list2); 
 
-        // When "Submit" button clicked, label text is set to the option selected
-        b2.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String data = "";
-                if(list.getSelectedIndex() != -1) {
-                    data = list.getSelectedValue();
-                    label.setText(data);
-                }
-            }
-        });
+        DefaultListModel<String> l3 = new DefaultListModel<>();  
+        for (String s : sides) {
+            l3.addElement(s);
+        }
+        JList<String> list3 = new JList<>(l3);  
+        list3.setBounds(660, 100, 100, 110);  
+        f.add(list3); 
 
-
-        f.add(b2); f.add(label); f.add(panel);  
+    
+        f.add(panel);  
         f.setSize(1010,610);    
         f.setLayout(null);    
         f.setVisible(true);   
-    }  
-
-    public static void main(String args[])  
-    {  
-        new Manager_Home_Page();  
     }  
 }

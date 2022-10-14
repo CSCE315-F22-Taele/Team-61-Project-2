2d56a672fe324ac48e236410c062818e593bc979
Brandon Moon <brandonmoon@connect-172-31-60-122.vpn.tamu.edu>
2022-10-13 19:19:03 -0500

    added label for Item ID text box and repositioned them

--- Manager_Home_Page.java
+++ Manager_Home_Page.java
@@ -106,7 +106,12 @@ public class Manager_Home_Page {
 
         // Label to type in Item ID
         JTextArea updateLabel = new JTextArea();
-        updateLabel.setBounds(30, 500, 50, 20);
+        updateLabel.setBounds(20, 500, 50, 20);
+
+        JLabel updateTextLabel = new JLabel("Item ID");
+        updateTextLabel.setBounds(20, 470, 100, 30);
+        f.add(updateTextLabel);
+
         
         // After drop down menu items are selected and submit button is pressed, the values are stored and outputted on the frame
         submitButton.addActionListener(new ActionListener() {
@@ -152,7 +157,7 @@ public class Manager_Home_Page {
             }
         });
     
-        f.add(submitButton); f.add(queryLabel); f.add(totalLabel); f.add(updateLabel);
+        f.add(submitButton); f.add(queryLabel); f.add(totalLabel); f.add(updateLabel); 
 
         f.add(panel);  
         f.setSize(1010,610);    

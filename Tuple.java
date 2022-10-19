1eb3e00ec26be3bbc111a0ed4ba44ad40ba0184b
Brandon Moon <brandonmoon@MacBook-Pro-3.local>
2022-10-18 19:00:39 -0500

    created documentation for Tuple.java

--- Tuple.java
+++ Tuple.java
@@ -1,13 +1,24 @@
+/**
+ * This class is used to create an array of tuples within the Home_Page to store each side with it's corresponding amount
+ * @author Roee Belkin
+ */
 public class Tuple{
     public String side_name; 
     public Integer amount; 
 
-
+    /**
+     * The tuple constructor initalizes the attributes with their default values of an empty string and 0.
+     */
     Tuple(){
         side_name = "";
         amount = 0; 
     }
 
+    /**
+     * This constructor is used to create Tuple objects for a specific side with its' corresponding price.
+     * @param side
+     * @param amt
+     */
     Tuple(String side, Integer amt){
         side_name = side; 
         amount = amt; 
--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Clerk_Home_Page"},{"p":"<Unnamed>","l":"Home_Page"},{"p":"<Unnamed>","l":"jdbcpostgreSQL"},{"p":"<Unnamed>","l":"Manager_Home_Page"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Clerk_Home_Page"},{"p":"<Unnamed>","l":"Home_Page"},{"p":"<Unnamed>","l":"jdbcpostgreSQL"},{"p":"<Unnamed>","l":"Manager_Home_Page"},{"p":"<Unnamed>","l":"Tuple"}];updateSearchResults();
